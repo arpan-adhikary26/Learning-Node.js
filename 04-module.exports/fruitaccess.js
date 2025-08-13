fcdf2d88ea4df8394f5a3b991270445196c549ec
ARPAN ADHIKARY <arpanadhikary26@gmail.com>
2025-08-13 21:51:02 +0530

    sample code added

---
+++ 04-module.exports/fruitaccess.js
@@ -0,0 +1,5 @@
+let fruits = require("./fruits");
+
+console.log(fruits[0].name);
+console.log(fruits[1].name);
+console.log(fruits[2].name);
---
+++ 04-module.exports/fruits/index.js
@@ -0,0 +1,5 @@
+let apple = require("./apple");
+let banana = require("./banana");
+let orange = require("./orange");
+
+module.exports =[apple,banana,orange];
